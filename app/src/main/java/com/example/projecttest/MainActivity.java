90a272c2e6eea5bb80e24e15fef2c382a2e74917
tiennab <tiennab@bkav.com>
2020-02-04 18:06:55 +0700

    test CollapsiingToolbarLayout 04/02/2020 18:07

--- app/src/main/java/com/example/projecttest/MainActivity.java
+++ app/src/main/java/com/example/projecttest/MainActivity.java
@@ -5,7 +5,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,27 +27,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTitle("Hello !!!");
+
+//        final TextView textView = findViewById(R.id.text);
+//
+//        final CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.layout_collapsing);
+//        AppBarLayout appBarLayout = findViewById(R.id.layout_appbar);
+//        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+//            boolean isShow = true;
+//            int scrollRange = -1;
+//
+//            @Override
+//            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+//                if (scrollRange == -1) {
+//                    scrollRange = appBarLayout.getTotalScrollRange();
+//                }
+//                if (scrollRange + verticalOffset == 0) {
+////                    textView.setVisibility(View.GONE);
+//                    toolbar.setTitle("Điện Thoại");
+//                    isShow = true;
+//                } else if(isShow) {
+////                    textView.setVisibility(View.VISIBLE);
+//                    toolbar.setTitle(" ");//careful there should a space between double quote otherwise it wont work
+//                    isShow = false;
+//                }
+//            }
+//        });
+
         mRecyclerView = findViewById(R.id.recyclerview);
         mData = getResources().getStringArray(R.array.array);
         mList = Arrays.asList(mData);
         mAdapter = new TestAdapter(this, mList);
         mRecyclerView.setAdapter(mAdapter);
-//        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
 
-        final RecyclerViewFastScroller fastScroller = findViewById(R.id.fastscroller);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false) {
-            @Override
-            public void onLayoutCompleted(final RecyclerView.State state) {
-                super.onLayoutCompleted(state);
-                final int firstVisibleItemPosition = findFirstVisibleItemPosition();
-                final int lastVisibleItemPosition = findLastVisibleItemPosition();
-                int itemsShown = lastVisibleItemPosition - firstVisibleItemPosition + 1;
-                //if all items are shown, hide the fast-scroller
-                fastScroller.setVisibility(mAdapter.getItemCount() > itemsShown ? View.VISIBLE : View.GONE);
-            }
-        });
-        fastScroller.setRecyclerView(mRecyclerView);
-        fastScroller.setViewsToUse(R.layout.recycler_view_fast_scroller__fast_scroller, R.id.fastscroller_bubble, R.id.fastscroller_handle);
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/projecttest/TestAdapter.java
+++ app/src/main/java/com/example/projecttest/TestAdapter.java
@@ -19,62 +19,15 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 
-public class TestAdapter extends RecyclerView.Adapter<TestAdapter.TestViewHolder> implements /*SectionIndexer*/ RecyclerViewFastScroller.BubbleTextGetter {
+public class TestAdapter extends RecyclerView.Adapter<TestAdapter.TestViewHolder> {
 
-    HashMap<String, Integer> mMapIndex;
     List<String> mList;
-    String[] mSections;
     Context mContext;
 
     public TestAdapter(Context context,  List<String> data) {
         this.mList = data;
         this.mContext = context;
-
-//        mMapIndex = new LinkedHashMap<>();
-//        for (int i = 0; i < mList.size(); i++) {
-//            String s = mList.get(i);
-//            String ch = s.substring(0,1);
-//            ch = ch.toUpperCase(Locale.US);
-//
-//            mMapIndex.put(ch, i);
-//        }
-//        Set<String> sectionLetters = mMapIndex.keySet();
-//        ArrayList<String> sectionList = new ArrayList<>(sectionLetters);
-//        Collections.sort(sectionList);
-//        mSections = new String[sectionList.size()];
-//        sectionList.toArray(mSections);
     }
-
-//    @Override
-//    public View getView(int position, View convertView, ViewGroup parent) {
-//        ViewHolder viewHolder;
-//        if (convertView == null) {
-//            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_layout, parent, false);
-//            viewHolder = new ViewHolder();
-//            viewHolder.tvTitle = convertView.findViewById(R.id.txt_title);
-//
-//            convertView.setTag(viewHolder);
-//        } else {
-//            viewHolder = (ViewHolder) convertView.getTag();
-//        }
-//        viewHolder.tvTitle.setText(mList.get(position));
-//        return convertView;
-//    }
-
-//    @Override
-//    public Object[] getSections() {
-//        return mSections;
-//    }
-//
-//    @Override
-//    public int getPositionForSection(int i) {
-//        return mMapIndex.get(mSections[i]);
-//    }
-//
-//    @Override
-//    public int getSectionForPosition(int i) {
-//        return 0;
-//    }
 
     @NonNull
     @Override
@@ -91,11 +44,6 @@ public class TestAdapter extends RecyclerView.Adapter<TestAdapter.TestViewHolder
     @Override
     public int getItemCount() {
         return mList.size();
-    }
-
-    @Override
-    public String getTextToShowInBubble(int pos) {
-        return Character.toString(mList.get(pos).charAt(0));
     }
 
     // class
